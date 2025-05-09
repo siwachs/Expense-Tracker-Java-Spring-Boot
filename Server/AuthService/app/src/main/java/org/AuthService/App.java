b4c66f548a4fe0e5dc878621c5cc1b89b3a6e5ca
siwachs <ssiwach.8888@gmail.com>
2025-05-09 14:57:51 +0530

    Update Project Dependencies

---
+++ Server/AuthService/app/src/main/java/org/AuthService/App.java
@@ -0,0 +1,10 @@
+package org.AuthService;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
